bf244543172dfb6778480aeea3b96ffa6d2682d0
LWAZILUBISI <lwaziluigi@gmail.com>
2026-03-15 01:38:59 +0200

    Temples page made

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+const menuButton = document.querySelector('#menuButton');
+const navMenu = document.querySelector('#navMenu');
+
+menuButton.addEventListener('click', () => {
+navMenu.classList.toggle('show');
+
+if(menuButton.textContent === '☰'){
+menuButton.textContent = 'X';
+}else{
+menuButton.textContent = '☰';
+}
+});
+
+
+// Footer Dynamic Year
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+// Last Modified
+document.querySelector('#lastModified').textContent = document.lastModified;
